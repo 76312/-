d0903aca6069a2bd06725a63de968b02078ec2e7
76312 <53069964+76312@users.noreply.github.com>
2020-01-28 18:38:45 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace 黑白棋
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "黑棋数：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "白棋数：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "棋子数：";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(450, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(172, 417);
+            this.panel1.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(62, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(11, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(62, 30);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(11, 12);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(62, 60);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(11, 12);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LimeGreen;
+            this.ClientSize = new System.Drawing.Size(634, 441);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "五子棋";
+            this.LocationChanged += new System.EventHandler(this.Form1_LocationChanged);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,370 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 黑白棋
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        int temp = 0;
+        int pos_x = 0;//上一步坐标
+        int pos_y = 0;
+
+        int num_black = 0;//黑棋数
+        int num_write = 0;//白棋数
+        int successer = 0;//胜利者，0表示未分胜负，1为黑色方，2为白色方，-1代表棋盘已满
+
+
+
+        int[,] map = new int[15,15];
+        public Form1()
+        {
+            InitializeComponent();
+            this.Width = 650;
+            this.Height = 480;
+            
+            //初始化棋盘
+            for(int i=0;i<15;i++)
+            {
+                for(int j = 0; j < 15; j++)
+                {
+                    map[i,j] = 0; 
+                }
+            }
+        }
+
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            g = this.CreateGraphics();
+            Pen pen_black = new Pen(Color.Black);
+            pen_black.Width = 2;
+            for (int i = 0; i < 15; i++)
+            {
+                g.DrawLine(pen_black, 10, 10 + 30 * i, 430, 10 + 30 * i);
+                g.DrawLine(pen_black, 10 + 30 * i, 10, 10 + 30 * i, 430);
+                for (int j = 0; j < 15; j++)
+                {
+
+                    if (map[i, j] == 1)
+                    {
+                        g.FillEllipse(Brushes.Black, new Rectangle(i * 30, j * 30 , 20, 20));
+                    }
+                    else if (map[i, j] == 2)
+                    {
+                        g.FillEllipse(Brushes.White, new Rectangle(i * 30, j * 30, 20, 20));
+                    }
+
+                }
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            Point p = Control.MousePosition;
+            p = this.PointToClient(p);
+            foundPoint(ref p);
+            this.RefreshMap();
+            successer = JudgeResult();
+            //if(p.X<=450&&p.Y<=450)
+            //{
+            //    if (temp == 0)
+            //    {
+            //        temp = 1;
+            //        g.FillEllipse(Brushes.Black, new Rectangle(p.X - 25, p.Y - 25, 50, 50));
+            //    }
+            //    else
+            //    {
+            //        temp = 0;
+            //        g.FillEllipse(Brushes.White, new Rectangle(p.X - 25, p.Y - 25, 50, 50));
+            //    }
+            //}
+        }
+
+        //判断点击的下棋地址
+        void foundPoint(ref Point p)
+        {
+            int x = p.X/30;
+            int y = p.Y/30;
+
+            Console.WriteLine("坐标："+ x +','+y);
+            try
+            {
+                if (map[x, y] == 0)
+                {
+                    pos_x = x;
+                    pos_y = y;
+                    if (temp == 0)
+                    {
+                        map[x, y] = 1;
+                        label4.Text = Convert.ToString(Convert.ToInt32(label4.Text) + 1);
+                        label6.Text = Convert.ToString(Convert.ToInt32(label6.Text) + 1);
+                        temp = 1;
+                    }
+                    else
+                    {
+                        map[x, y] = 2;
+                        label5.Text = Convert.ToString(Convert.ToInt32(label5.Text) + 1);
+                        label6.Text = Convert.ToString(Convert.ToInt32(label6.Text) + 1);
+                        temp = 0;
+                    }
+                }
+            }
+            catch { }
+        }
+
+        //刷新界面
+        void RefreshMap()
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                for (int j = 0; j < 15; j++)
+                {
+
+                    if (map[i, j] == 1)
+                    {
+                        g.FillEllipse(Brushes.Black, new Rectangle(i * 30, j * 30, 20, 20));
+                    }
+                    else if (map[i, j] == 2)
+                    {
+                        g.FillEllipse(Brushes.White, new Rectangle(i * 30, j * 30, 20, 20));
+                    }
+
+                }
+            }
+        }
+
+        private void Form1_LocationChanged(object sender, EventArgs e)
+        {
+            this.RefreshMap();
+        }
+
+
+        //判断胜负；
+        private int JudgeResult()
+        {
+            //判断竖列
+            if (pos_y < 11 &&
+                map[pos_x, pos_y + 4] == map[pos_x, pos_y] &&
+                map[pos_x, pos_y + 3] == map[pos_x, pos_y] &&
+                map[pos_x, pos_y + 2] == map[pos_x, pos_y] &&
+                map[pos_x, pos_y + 1] == map[pos_x, pos_y])
+            {
+                EndToGame();
+            }
+            else if (pos_y < 12 && pos_y > 0 &&
+                 map[pos_x, pos_y - 1] == map[pos_x, pos_y] &&
+                 map[pos_x, pos_y + 3] == map[pos_x, pos_y] &&
+                 map[pos_x, pos_y + 2] == map[pos_x, pos_y] &&
+                 map[pos_x, pos_y + 1] == map[pos_x, pos_y])
+            {
+                EndToGame();
+            }
+            else if (pos_y < 13 && pos_y > 1 &&
+                 map[pos_x, pos_y - 1] == map[pos_x, pos_y] &&
+                 map[pos_x, pos_y - 2] == map[pos_x, pos_y] &&
+                 map[pos_x, pos_y + 2] == map[pos_x, pos_y] &&
+                 map[pos_x, pos_y + 1] == map[pos_x, pos_y])
+            {
+                EndToGame();
+            }
+            else if (pos_y < 14 && pos_y > 2 &&
+                 map[pos_x, pos_y - 1] == map[pos_x, pos_y] &&
+                 map[pos_x, pos_y - 3] == map[pos_x, pos_y] &&
+                 map[pos_x, pos_y - 2] == map[pos_x, pos_y] &&
+                 map[pos_x, pos_y + 1] == map[pos_x, pos_y])
+            {
+                EndToGame();
+            }
+            else if (pos_y > 3 &&
+                 map[pos_x, pos_y - 4] == map[pos_x, pos_y] &&
+                 map[pos_x, pos_y - 3] == map[pos_x, pos_y] &&
+                 map[pos_x, pos_y - 2] == map[pos_x, pos_y] &&
+                 map[pos_x, pos_y - 1] == map[pos_x, pos_y])
+            {
+                EndToGame();
+            }
+
+            //判断横列
+            else if (pos_x < 11 &&
+                 map[pos_x + 1, pos_y] == map[pos_x, pos_y] &&
+                 map[pos_x + 2, pos_y] == map[pos_x, pos_y] &&
+                 map[pos_x + 3, pos_y] == map[pos_x, pos_y] &&
+                 map[pos_x + 4, pos_y] == map[pos_x, pos_y])
+            {
+                EndToGame();
+            }
+            else if (pos_x < 12 && pos_x > 0 &&
+                 map[pos_x + 1, pos_y] == map[pos_x, pos_y] &&
+                 map[pos_x + 2, pos_y] == map[pos_x, pos_y] &&
+                 map[pos_x + 3, pos_y] == map[pos_x, pos_y] &&
+                 map[pos_x - 1, pos_y] == map[pos_x, pos_y])
+            {
+                EndToGame();
+            }
+            else if (pos_x < 13 && pos_x > 1 &&
+                map[pos_x + 1, pos_y] == map[pos_x, pos_y] &&
+                map[pos_x + 2, pos_y] == map[pos_x, pos_y] &&
+                map[pos_x - 1, pos_y] == map[pos_x, pos_y] &&
+                map[pos_x - 2, pos_y] == map[pos_x, pos_y])
+            {
+                EndToGame();
+            }
+            else if (pos_x < 14 && pos_x > 2 &&
+                map[pos_x + 1, pos_y] == map[pos_x, pos_y] &&
+                map[pos_x - 3, pos_y] == map[pos_x, pos_y] &&
+                map[pos_x - 1, pos_y] == map[pos_x, pos_y] &&
+                map[pos_x - 2, pos_y] == map[pos_x, pos_y])
+            {
+                EndToGame();
+            }
+            else if (pos_x > 3 &&
+                map[pos_x - 3, pos_y] == map[pos_x, pos_y] &&
+                map[pos_x - 4, pos_y] == map[pos_x, pos_y] &&
+                map[pos_x - 1, pos_y] == map[pos_x, pos_y] &&
+                map[pos_x - 2, pos_y] == map[pos_x, pos_y])
+            {
+                EndToGame();
+            }
+
+            //判断左斜
+            else if (pos_x < 11 && pos_y < 11 &&
+                map[pos_x + 1, pos_y + 1] == map[pos_x, pos_y] &&
+                map[pos_x + 2, pos_y + 2] == map[pos_x, pos_y] &&
+                map[pos_x + 3, pos_y + 3] == map[pos_x, pos_y] &&
+                map[pos_x + 4, pos_y + 4] == map[pos_x, pos_y])
+            {
+                EndToGame();
+            }
+            else if (pos_x < 12 && pos_y < 12 && pos_x > 0 && pos_y > 0 &&
+                map[pos_x + 1, pos_y + 1] == map[pos_x, pos_y] &&
+                map[pos_x + 2, pos_y + 2] == map[pos_x, pos_y] &&
+                map[pos_x + 3, pos_y + 3] == map[pos_x, pos_y] &&
+                map[pos_x - 1, pos_y - 1] == map[pos_x, pos_y])
+            {
+                EndToGame();
+            }
+            else if (pos_x <13 && pos_y < 13 && pos_x > 1 && pos_y > 1 &&
+                map[pos_x + 1, pos_y + 1] == map[pos_x, pos_y] &&
+                map[pos_x + 2, pos_y + 2] == map[pos_x, pos_y] &&
+                map[pos_x - 1, pos_y - 1] == map[pos_x, pos_y] &&
+                map[pos_x - 2, pos_y - 2] == map[pos_x, pos_y])
+            {
+                EndToGame();
+            }
+            else if (pos_x < 14 && pos_y < 14 && pos_x > 2 && pos_y > 2 &&
+                map[pos_x - 1, pos_y - 1] == map[pos_x, pos_y] &&
+                map[pos_x - 2, pos_y - 2] == map[pos_x, pos_y] &&
+                map[pos_x - 3, pos_y - 3] == map[pos_x, pos_y] &&
+                map[pos_x + 1, pos_y + 1] == map[pos_x, pos_y])
+            {
+                EndToGame();
+            }
+            else if (pos_x > 3 && pos_y > 3 &&
+                map[pos_x - 1, pos_y - 1] == map[pos_x, pos_y] &&
+                map[pos_x - 2, pos_y - 2] == map[pos_x, pos_y] &&
+                map[pos_x - 3, pos_y - 3] == map[pos_x, pos_y] &&
+                map[pos_x - 4, pos_y - 4] == map[pos_x, pos_y])
+            {
+                EndToGame();
+            }
+
+            //判断右斜
+            else if (pos_x < 13 && 14-pos_y < 13 && pos_x > 1 && 14 - pos_y > 1 &&
+                map[pos_x + 1, pos_y - 1] == map[pos_x, pos_y] &&
+                map[pos_x + 2, pos_y - 2] == map[pos_x, pos_y] &&
+                map[pos_x - 1, pos_y + 1] == map[pos_x, pos_y] &&
+                map[pos_x - 2, pos_y + 2] == map[pos_x, pos_y])
+            {
+                EndToGame();
+            }
+            else if (pos_x < 14 && 14-pos_y < 14 && pos_x > 2 && 14 - pos_y > 2 &&
+                map[pos_x + 1, pos_y - 1] == map[pos_x, pos_y] &&
+                map[pos_x - 3, pos_y + 3] == map[pos_x, pos_y] &&
+                map[pos_x - 1, pos_y + 1] == map[pos_x, pos_y] &&
+                map[pos_x - 2, pos_y + 2] == map[pos_x, pos_y])
+            {
+                EndToGame();
+            }
+            else if (pos_x > 3 && 14 - pos_y > 3 &&
+                map[pos_x - 3, pos_y + 3] == map[pos_x, pos_y] &&
+                map[pos_x - 4, pos_y + 4] == map[pos_x, pos_y] &&
+                map[pos_x - 1, pos_y + 1] == map[pos_x, pos_y] &&
+                map[pos_x - 2, pos_y + 2] == map[pos_x, pos_y])
+            {
+                EndToGame();
+            }
+            else if (pos_x < 11 && 14 - pos_y < 11 &&
+                map[pos_x + 3, pos_y - 3] == map[pos_x, pos_y] &&
+                map[pos_x + 4, pos_y - 4] == map[pos_x, pos_y] &&
+                map[pos_x + 1, pos_y - 1] == map[pos_x, pos_y] &&
+                map[pos_x + 2, pos_y - 2] == map[pos_x, pos_y])
+            {
+                EndToGame();
+            }
+            else if (pos_x < 12 && 14-pos_y < 12 && pos_x > 1 && 14 - pos_y > 1 &&
+                map[pos_x - 1, pos_y + 1] == map[pos_x, pos_y] &&
+                map[pos_x + 3, pos_y - 3] == map[pos_x, pos_y] &&
+                map[pos_x + 1, pos_y - 1] == map[pos_x, pos_y] &&
+                map[pos_x + 2, pos_y - 2] == map[pos_x, pos_y])
+            {
+                EndToGame();
+            }
+            return 0;
+        }
+            //游戏结束
+            void EndToGame()
+        {
+            //显示游戏结果
+            if (map[pos_x, pos_y] == 1)
+            {
+                MessageBox.Show("游戏结束!" + '\n' + "黑色方胜利！");
+            }
+            else
+            { 
+                MessageBox.Show("游戏结束!" + '\n' + "白色方胜利！"); 
+            }
+            label4.Text = "0";
+            label5.Text = "0";
+            label6.Text = "0";
+            //刷新游戏界面
+            //初始化棋盘
+            for (int i = 0; i < 15; i++)
+            {
+                for (int j = 0; j < 15; j++)
+                {
+                    map[i, j] = 0;
+                }
+            }
+            g.Clear(Color.LimeGreen);
+            Pen pen_black = new Pen(Color.Black);
+            pen_black.Width = 2;
+            for (int i = 0; i < 15; i++)
+            {
+                g.DrawLine(pen_black, 10, 10 + 30 * i, 430, 10 + 30 * i);
+                g.DrawLine(pen_black, 10 + 30 * i, 10, 10 + 30 * i, 430);
+                for (int j = 0; j < 15; j++)
+                {
+
+                    if (map[i, j] == 1)
+                    {
+                        g.FillEllipse(Brushes.Black, new Rectangle(i * 30, j * 30, 20, 20));
+                    }
+                    else if (map[i, j] == 2)
+                    {
+                        g.FillEllipse(Brushes.White, new Rectangle(i * 30, j * 30, 20, 20));
+                    }
+
+                }
+            }
+            this.RefreshMap();
+        }
+    }
+}
